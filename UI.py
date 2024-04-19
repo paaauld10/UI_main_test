631f0a7e42f7bda4ee349f5be308838506bfb673
paaauld10 <89123946+paaauld10@users.noreply.github.com>
2024-04-19 19:45:41 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -162,37 +162,37 @@
 
 #################simple setup
 
-# from openai import OpenAI
-import streamlit as st
+# # from openai import OpenAI
+# import streamlit as st
 
-st.title("☃️ GAME NA CHATBOT")
-st.button("CLICK MO KO DITO PRE")
-st.file_uploader("DITO KA UPLOAD MADAM")
+# st.title("☃️ GAME NA CHATBOT")
+# st.button("CLICK MO KO DITO PRE")
+# st.file_uploader("DITO KA UPLOAD MADAM")
 
-# Initialize the chat messages history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
+# # Initialize the chat messages history
+# if "messages" not in st.session_state.keys():
+#     st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
 
-# Prompt for user input and save
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
+# # Prompt for user input and save
+# if prompt := st.chat_input():
+#     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# display the existing chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+# # display the existing chat messages
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.write(message["content"])
 
-# If last message is not from assistant, we need to generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    # Call LLM
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            r = OpenAI().chat.completions.create(
-                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-                model="gpt-3.5-turbo",
-            )
-            response = r.choices[0].message.content
-            st.write(response)
+# # If last message is not from assistant, we need to generate a new response
+# if st.session_state.messages[-1]["role"] != "assistant":
+#     # Call LLM
+#     with st.chat_message("assistant"):
+#         with st.spinner("Thinking..."):
+#             r = OpenAI().chat.completions.create(
+#                 messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+#                 model="gpt-3.5-turbo",
+#             )
+#             response = r.choices[0].message.content
+#             st.write(response)
 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+#     message = {"role": "assistant", "content": response}
+#     st.session_state.messages.append(message)
