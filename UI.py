04c9663577e436168a5deddaef67247242c3c889
paaauld10 <89123946+paaauld10@users.noreply.github.com>
2024-04-19 18:55:23 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,70 +1,70 @@
-import streamlit as st
-from openai import OpenAI
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-st.title("💬 Chatbot")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg) 
-
-
-
-
-
-##################simple setup
-
-# # from openai import OpenAI
 # import streamlit as st
+# from openai import OpenAI
 
-# st.title("☃️ GAME NA CHATBOT")
-# st.button("CLICK MO KO DITO PRE")
-# st.file_uploader("DITO KA UPLOAD MADAM")
+# with st.sidebar:
+#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+#     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+#     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-# # Initialize the chat messages history
-# if "messages" not in st.session_state.keys():
-#     st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
+# st.title("💬 Chatbot")
 
-# # Prompt for user input and save
+# if "messages" not in st.session_state:
+#     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+# for msg in st.session_state.messages:
+#     st.chat_message(msg["role"]).write(msg["content"])
+
 # if prompt := st.chat_input():
+#     if not openai_api_key:
+#         st.info("Please add your OpenAI API key to continue.")
+#         st.stop()
+
+#     client = OpenAI(api_key=openai_api_key)
 #     st.session_state.messages.append({"role": "user", "content": prompt})
+#     st.chat_message("user").write(prompt)
+#     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+#     msg = response.choices[0].message.content
+#     st.session_state.messages.append({"role": "assistant", "content": msg})
+#     st.chat_message("assistant").write(msg) 
 
-# # display the existing chat messages
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.write(message["content"])
 
-# # If last message is not from assistant, we need to generate a new response
-# if st.session_state.messages[-1]["role"] != "assistant":
-#     # Call LLM
-#     with st.chat_message("assistant"):
-#         with st.spinner("Thinking..."):
-#             r = OpenAI().chat.completions.create(
-#                 messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-#                 model="gpt-3.5-turbo",
-#             )
-#             response = r.choices[0].message.content
-#             st.write(response)
 
-#     message = {"role": "assistant", "content": response}
-#     st.session_state.messages.append(message)
+
+
+#################simple setup
+
+# from openai import OpenAI
+import streamlit as st
+
+st.title("☃️ GAME NA CHATBOT")
+st.button("CLICK MO KO DITO PRE")
+st.file_uploader("DITO KA UPLOAD MADAM")
+
+# Initialize the chat messages history
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help?"}]
+
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# display the existing chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, we need to generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    # Call LLM
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            r = OpenAI().chat.completions.create(
+                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+                model="gpt-3.5-turbo",
+            )
+            response = r.choices[0].message.content
+            st.write(response)
+
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
